2dca25010598a92e8b6766ca3a778e3bfec5770d
Artick <jjam1190@gmail.com>
2020-08-14 21:16:57 -0500

    Conect with an API

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="Header-main">
       <div className="Header-logo">
         <h1>
-          <a href="/">
+          <a href="#">
             100tifi.co
           </a>
         </h1>
